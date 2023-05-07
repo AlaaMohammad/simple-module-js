38c7be089b0c026175a1ac72159270399d89262a
Alaa Mohammad <alaa.mohammadhb@gmail.com>
2023-05-07 23:35:41 +0300

    simple-module js

---
+++ App.mjs
@@ -0,0 +1,6 @@
+import User from './User.mjs'
+
+let obj =  new User();
+obj.getUser();
+
+//we use mjs extension because we are dealing with module 
---
+++ User.mjs
@@ -0,0 +1,8 @@
+class User {
+
+   getUser(){
+        console.log('test')}
+    
+}
+
+export default User;
